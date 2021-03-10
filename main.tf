043414ee03a3a877fad45d3f5350d2ac724fdb28
thomas-tank-corp <tharris@hashicorp.com>
2021-03-10 09:34:30 +0000

    first

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project     = var.project
+  region      = "europe-west2"
+  zone        = "europe-west2a"
+}
+
+
+resource "google_compute_instance" "default" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = "europe-west2-a"
+
+  tags = ["test", "git"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
